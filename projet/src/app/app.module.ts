d73ae1db125f9d7afdb84fa916d334fcc6185712
seb_guillemin <seb_guillemin@users.noreply.github.com>
2020-11-27 21:29:16 +0100

    Maj

--- projet/src/app/app.module.ts
+++ projet/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnectionComponent } from './connection/connection.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnectionComponent
+    ConnectionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
