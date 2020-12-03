211dcd2fd5d018853a227dc0e50ac3345f1e4a1f
Death <xiaoxiao921@hotmail.fr>
2020-12-03 02:34:03 +0100

    string-enum UserRole, utilise par le model User

--- projet/src/app/shared/model/user.model.ts
+++ projet/src/app/shared/model/user.model.ts
@@ -1,7 +1,8 @@
 import { ModelFormInterface } from './modelFormInterface';
+import { UserRole } from 'src/app/shared/model/UserRole';
 
 export class User implements ModelFormInterface {
-    constructor(public username?: string, public password?: string,  private _role?: string) { }
+    constructor(public username?: string, public password?: string,  public role?: UserRole) { }
 
     getFormFields(): Array<string> {
         return ["username", "password"];
