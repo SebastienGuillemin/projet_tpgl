729035b3d46e45ddd77f66720aeb3c74c0543940
Death <xiaoxiao921@hotmail.fr>
2020-12-03 02:34:57 +0100

    lecture fichier User db

--- projet/express_server/Services/UserConnection/index.ts
+++ projet/express_server/Services/UserConnection/index.ts
@@ -1,17 +1,17 @@
-import { invalid } from '@angular/compiler/src/render3/view/util';
 import * as express from 'express';
+import { UserRole } from 'src/app/shared/model/UserRole';
+import { User } from 'src/app/shared/model/user.model';
+import { UserDb } from '../UserDb';
 
 export class UserConnectionService {
     connection(req: express.Request) {
-        var username = req.body.username;
-        var password = req.body.password;
+        var user = req.body as User;
+        return this.checkUser(user);
+    }
 
-        //il faudra connecter à la bd et retourner un JSON contenant le rôle.
-        if (username == "admin" && password == "admin")
-            return "ok";
-        else if (username == "user" && password == "user")
-            return "ok";
-        else
-            return "invalid";
-    };
+    // on regarde si l'user existe dans la db, si oui, alors on retourne son role pour ensuite rediriger vers la bonne page
+    checkUser(user: User): UserRole {
+        var userDb = new UserDb();
+        return userDb.getUserRole(user);
+    }
 }
