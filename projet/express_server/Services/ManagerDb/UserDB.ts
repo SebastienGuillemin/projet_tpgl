89f1b285eea5c74f70c2f7bfda0f3aae1ebc6000
Jérémy Tridard <60389402+jeremwy@users.noreply.github.com>
2020-12-08 22:31:44 +0100

    Création managerDB qui englobe userDB

---
+++ projet/express_server/Services/ManagerDb/UserDB.ts
@@ -0,0 +1,16 @@
+import * as express from 'express';
+import { UserRole } from 'src/app/shared/model/UserRole';
+import { User } from 'src/app/shared/model/user.model';
+
+import fs = require('fs');
+
+export class UserDB {
+    private users_file_path: string = 'express_server/database/user.json';
+
+    getUserRole(user: User): UserRole {
+        // des node modules existe pour lire des gros fichier json de façon efficace, mais ici pas besoin
+        var users: Array<User> = JSON.parse(fs.readFileSync(this.users_file_path, 'utf8'));
+
+        return users.find(u => u.username == user.username)?.role;
+    }
+}
--- projet/express_server/Services/UserConnection/index.ts
+++ projet/express_server/Services/UserConnection/index.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import { UserRole } from 'src/app/shared/model/UserRole';
 import { User } from 'src/app/shared/model/user.model';
-import { UserDb } from '../UserDb';
+import { ManagerDB } from '../ManagerDB';
 
 export class UserConnectionService {
     connection(req: express.Request) {
@@ -11,7 +11,7 @@ export class UserConnectionService {
 
     // on regarde si l'user existe dans la db, si oui, alors on retourne son role pour ensuite rediriger vers la bonne page
     checkUser(user: User): UserRole {
-        var userDb = new UserDb();
-        return userDb.getUserRole(user);
+        var managerDB = new ManagerDB();
+        return managerDB.getUserRole(user);
     }
 }
