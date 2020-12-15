1e85af1ea99d27de5170a6b303c347778a44eb4e
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2020-12-15 20:57:49 +0100

    fix tests

--- projet/src/app/fournir-donnees/fournir-donnees-cereales/fournir-donnees-cereales.component.spec.ts
+++ projet/src/app/fournir-donnees/fournir-donnees-cereales/fournir-donnees-cereales.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from 'src/app/app-routing.module';
 
 import { FournirDonneesCerealesComponent } from './fournir-donnees-cereales.component';
 
@@ -8,6 +11,10 @@ describe('FournirDonneesCerealesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(routes),
+        HttpClientTestingModule,
+      ],
       declarations: [ FournirDonneesCerealesComponent ]
     })
     .compileComponents();
