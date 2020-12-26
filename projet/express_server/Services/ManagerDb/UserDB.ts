a1f610fae90f5cd9dee8db9527198b965ed7ee7f
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2020-12-26 18:45:21 +0100

    code reuse

--- projet/express_server/Services/ManagerDb/UserDB.ts
+++ projet/express_server/Services/ManagerDb/UserDB.ts
@@ -1,22 +1,17 @@
-import * as express from 'express';
 import { User, UserRole } from 'src/app/shared/model/user.model';
-
-import fs = require('fs');
+import { JsonDbParser } from '../../Util/JsonDbParser';
 
 export class UserDB {
     private users_file_path: string = 'express_server/database/user.json';
 
-    getUserRole(user: User): UserRole {
-        //des node modules existe pour lire des gros fichier json de façon efficace, mais ici pas besoin
-        var users: Array<User> = JSON.parse(fs.readFileSync(this.users_file_path, 'utf8'));
-
-        return users.find(u => u.username == user.username)?.role;
-    }
-
     getUser(user: User): User {
         //des node modules existe pour lire des gros fichier json de façon efficace, mais ici pas besoin
-        var users: Array<User> = JSON.parse(fs.readFileSync(this.users_file_path, 'utf8'));
+        var users: Array<User> = JsonDbParser.read<User>(this.users_file_path);
 
         return users.find(u => u.username === user.username);
+    }
+
+    getUserRole(user: User): UserRole {
+        return this.getUser(user)?.role;
     }
 }
