ed99247fc2d9b82595c7c2aa0a1e8a9054736829
seb_guillemin <seb_guillemin@users.noreply.github.com>
2020-12-26 18:56:55 +0100

    Test de la classe matériel

--- projet/src/app/shared/model/cereale.model.ts
+++ projet/src/app/shared/model/cereale.model.ts
@@ -1,17 +1,19 @@
 import { ModelFormInterface } from './modelFormInterface';
 
 export class Cereale implements ModelFormInterface {
-    constructor (               
-        public num?: number, 
+    constructor(
+        public num?: number,
         public type?: string,
         public poids?: number,
         public qualite?: string,
         public acheminement?: string
     ) { }                                           //Il faut que les attributs soient publiques pour que JS puisse directement créer l'instance à partir de JSON (après lecture en BD).
-    
+
 
     //permet de retourner la liste des villes de l'acheminenemt.
     getAcheminementList(): string[] {
+        if (this.acheminement == "")
+            return [];
         let temp = this.acheminement.replace(' ', '');
         let tabAcheminement = temp.split(',');
         return tabAcheminement;
