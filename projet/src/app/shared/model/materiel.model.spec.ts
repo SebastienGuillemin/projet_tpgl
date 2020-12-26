29ac479fe1a81a8bbf619621b223410064015716
seb_guillemin <seb_guillemin@users.noreply.github.com>
2020-12-26 17:40:01 +0100

    ajout d'un test materiel

--- projet/src/app/shared/model/materiel.model.spec.ts
+++ projet/src/app/shared/model/materiel.model.spec.ts
@@ -1,6 +1,12 @@
 import { Materiel } from './materiel.model';
 
 describe('Materiel', () => {
+  let materiel: Materiel;
+
+  beforeEach(() => {
+    materiel = new Materiel();
+  });
+
   it('should create an instance', () => {
     expect(new Materiel()).toBeTruthy();
   });
