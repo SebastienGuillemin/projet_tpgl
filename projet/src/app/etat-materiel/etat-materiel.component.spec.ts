47b9ec519da91fac41794a2b559a4a987609c374
seb_guillemin <seb_guillemin@users.noreply.github.com>
2021-01-02 18:02:13 +0100

    etat materiel test

--- projet/src/app/etat-materiel/etat-materiel.component.spec.ts
+++ projet/src/app/etat-materiel/etat-materiel.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { routes } from '../app-routing.module';
 import { Cellule } from '../shared/model/materiels/cellule.model';
+import { ConnectionService } from '../shared/services/connection.service';
 import { MaterielService } from '../shared/services/materiel.service';
 
 import { EtatMaterielComponent } from './etat-materiel.component';
@@ -39,6 +40,12 @@ describe('EtatMaterielComponent', () => {
   });
 
   it('Doit récupérer les données depuis le serveur', () => {
+    //(inutile d'appeler ngOnInit() car déjà appelé une fois à la création du composant)
+    //Cas où l'utilisateur n'est pas connecté :
+    let reqConnection = httpTestingController.expectOne(ConnectionService.ApiUrl);  //Test si l'utilisateur est connecté.
+    reqConnection.flush("Non connecté", { status: 401, statusText: "Utilisateur non connecté" });
+    expect(component.materiels).toEqual([]);
+
     //Cas où un tableau vide est retourné :
     let req = httpTestingController.expectOne(MaterielService.GetApiUrl);
     req.flush([]);
